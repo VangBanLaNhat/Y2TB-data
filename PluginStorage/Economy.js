2fbd8021059fb8c59e1119c37556e8ab41d3807f
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-02-18 22:04:21 +0700

    Update Economy.js

--- PluginStorage/Economy.js
+++ PluginStorage/Economy.js
@@ -1,5 +1,5 @@
 function init(){
-    console.log(global.plugins);
+    console.log(global.plugins.Help);
     !global.data.economyConfig ? global.data.economyConfig = {icon: "Ꮙ"}:"";
     !global.data.economy ? global.data.economy = {}:"";
     return{

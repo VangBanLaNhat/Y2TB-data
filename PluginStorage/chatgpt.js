14ea3e0e65ce55a6706e713034383e5f43f5a0fe
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-03-04 22:08:58 +0700

    Add files via upload

--- PluginStorage/chatgpt.js
+++ PluginStorage/chatgpt.js
@@ -62,6 +62,7 @@ async function main(data, api) {
 			"role": "user",
 			"content": data.body
 		});*/
+		console.log(data.body);
 		let api_res = await openai.createCompletion({
 			model: "text-davinci-003",
 			prompt: data.body,

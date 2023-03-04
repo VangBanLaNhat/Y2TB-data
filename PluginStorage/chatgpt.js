964de55fd53be8308be804668dc4c649099753a8
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-03-04 09:07:58 +0700

    Update chatgpt.js

--- PluginStorage/chatgpt.js
+++ PluginStorage/chatgpt.js
@@ -46,11 +46,16 @@ async function main(data, api) {
 		})
 		
 		console.log(api_res.data.choices[0].text);
-		api.sendMessage(api_res.data.choices[0].text.toString(), data.threadID, (e)=>{console.log(e)}, data.senderID);
+		api.sendMessage(solver(api_res.data.choices[0].text.toString()), data.threadID, (e)=>{console.log(e)}, data.senderID);
 	} catch(e){
 		console.error("ChatGPT", e);
 		api.sendMessage(e, data.threadID, data.senderID);
 	}
+}
+
+function solver(s){
+	while(s[0] == '\n') s.replace('\n', '');
+	return s;
 }
 
 function ensureExists(path, mask) {

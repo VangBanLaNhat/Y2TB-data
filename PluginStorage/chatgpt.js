d729a6e79fc2c0a998a123c0c51d6b0572aebd68
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-03-05 16:45:56 +0700

    Add files via upload

--- PluginStorage/chatgpt.js
+++ PluginStorage/chatgpt.js
@@ -112,6 +112,7 @@ async function main(data, api, ii) {
 		global.data.openai.timeout.chatgpt[data.threadID] = setTimeout(function() {
 			if(global.data.openai.timeout.chatgpt[data.threadID]){
 				global.data.openai.chatgpt[data.threadID] = [];
+				delete global.data.openai.timeout.chatgpt[data.threadID]
 				api.sendMessage("Deleted chat data with ChatGPT in this Thread", data.threadID, data.messageID);
 			}
 		}, 1*30*60);
@@ -147,6 +148,7 @@ function del(data, api){
 	!global.data.openai ? global.data.openai = {}:"";
 	!global.data.openai.chatgpt ? global.data.openai.chatgpt = {}:"";
 	global.data.openai.chatgpt[data.threadID] = [];
+	delete global.data.openai.timeout.chatgpt[data.threadID];
 	api.sendMessage("Deleted chat data with ChatGPT in this Thread", data.threadID, data.messageID);
 }
 

b5d163406b0e017c68782e042fa84fd5d349c2a4
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-03-05 17:19:59 +0700

    Add files via upload

--- chatgpt.js
+++ chatgpt.js
@@ -22,6 +22,21 @@ function init() {
 					"en_US": "gpt hi"
 				}
 			},
+			"gptv1": {
+				"help": {
+					"vi_VN": "<msg>",
+					"en_US": "<msg>"
+				},
+				"tag": {
+					"vi_VN": "Chat với ChatGPT",
+					"en_US": "Chat with ChatGPT"
+				},
+				"mainFunc": "mainv1",
+				"example": {
+					"vi_VN": "gpt hi",
+					"en_US": "gpt hi"
+				}
+			},
 			"gptdel": {
 				"help": {
 					"vi_VN": "",
@@ -46,8 +61,8 @@ function init() {
 	}
 }
 
-async function main(data, api) {
-	if(data.body = "") return api.sendMessage("Please enter the input!", data.threadID, data.messageID);
+async function mainv1(data, api) {
+	if(data.body == "") return api.sendMessage("Please enter the input!", data.threadID, data.messageID);
 	!global.data.openai ? global.data.openai = {}:"";
 	!global.data.openai.chatgpt ? global.data.openai.chatgpt = {}:"";
 	!global.data.openai.chatgpt[data.threadID] ? global.data.openai.chatgpt[data.threadID] = []:"";
@@ -78,10 +93,62 @@ async function main(data, api) {
 	}
 }
 
+async function main(data, api, ii) {
+	if(data.body == "") return api.sendMessage("Please enter the input!", data.threadID, data.messageID);
+	!global.data.openai ? global.data.openai = {}:"";
+	!global.data.openai.chatgpt ? global.data.openai.chatgpt = {}:"";
+	!global.data.openai.chatgpt[data.threadID] ? global.data.openai.chatgpt[data.threadID] = []:"";
+	
+	!global.openai ? global.openai = {}:"";
+	!global.openai.timeout ? global.openai.timeout = {}:"";
+	!global.openai.timeout.chatgpt ? global.openai.timeout.chatgpt = {}:"";
+	
+	const { Configuration, OpenAIApi } = require("openai");
+	const configuration = new Configuration({
+		apiKey: "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",
+	});
+	const openai = new OpenAIApi(configuration);
+	
+	if(!global.openai.timeout.chatgpt[data.threadID])
+		global.openai.timeout.chatgpt[data.threadID] = setTimeout(function() {
+				global.data.openai.chatgpt[data.threadID] = [];
+				api.sendMessage("Deleted chat data with ChatGPT in this Thread", data.threadID, data.messageID);
+		}, 1*30*60*1000);
+	
+	try{
+		global.data.openai.chatgpt[data.threadID].push({
+			"role": "user",
+			"content": data.body
+		});
+		//console.log(data.body);
+		let api_res = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: global.data.openai.chatgpt[data.threadID],
+			max_tokens: 2049 - data.body.length
+		})
+		
+		if(api_res.data.choices[0].message.content.toString().length <= 1000){
+			global.data.openai.chatgpt[data.threadID].push(api_res.data.choices[0].message);
+		}
+		//console.log(api_res.data.choices[0].text);
+		api.sendMessage(api_res.data.choices[0].message.content.toString(), data.threadID, data.messageID);
+	} catch(e){
+		console.error("ChatGPT", e);
+		ii = !ii?0:ii;
+		if(ii > 3) return api.sendMessage(e, data.threadID, data.messageID);
+		global.data.openai.chatgpt[data.threadID].shift();
+		return main(data, api, ii+1);
+		global.data.openai.chatgpt[data.threadID].shift();
+	}
+}
+
+
 function del(data, api){
 	!global.data.openai ? global.data.openai = {}:"";
 	!global.data.openai.chatgpt ? global.data.openai.chatgpt = {}:"";
 	global.data.openai.chatgpt[data.threadID] = [];
+	if(global.openai.timeout.chatgpt[data.threadID])
+		clearTimeout(global.openai.timeout.chatgpt[data.threadID]);
 	api.sendMessage("Deleted chat data with ChatGPT in this Thread", data.threadID, data.messageID);
 }
 
@@ -105,5 +172,6 @@ function ensureExists(path, mask) {
 module.exports = {
 	del,
 	main,
+	mainv1,
 	init
 };

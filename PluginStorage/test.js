d3e780a58659353c59a5fa2129db97cc9181f602
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2023-05-15 09:46:13 +0700

    Add files via upload

--- PluginStorage/test.js
+++ PluginStorage/test.js
@@ -1,39 +1,113 @@
-function init(){
-    return{
-        "pluginName": "test",
-        "pluginMain": "test.js",
+function init() {
+    return {
+        "pluginName": "Download Tiktok",
+        "pluginMain": "DLTiktok.js",
         "desc": {
-            "vi_VN": "test",
-            "en_US": "test"
+            "vi_VN": "Tải video Tiktok từ link!",
+            "en_US": "Download Tiktok videos from the link!",
         },
         "commandList": {
-			"123": {
+            "tik": {
                 "help": {
-                    "vi_VN": "",
-                    "en_US": ""
+                    "vi_VN": "<link video tiktok>",
+                    "en_US": "<link video tiktok>",
                 },
                 "tag": {
-                    "vi_VN": "xD",
-                    "en_US": "xD"
+                    "vi_VN": "Tải video Tiktok từ link!",
+                    "en_US": "Download Tiktok videos from the link!",
                 },
-                "mainFunc": "test"
+                "mainFunc": "main",
+                "example": {
+                    "vi_VN": "https://vt.tiktok.com/ZSL145Wsd/",
+                    "en_US": "https://vt.tiktok.com/ZSL145Wsd/",
+                }
             }
         },
+        "author": "Yuuki",
+        "version": "0.0.1",
         "nodeDepends": {
-            "axios": ""
+            "tiktok-video-downloader": ""
         },
-        "author": "HyTommy",
-        "version": "0.0.1"
+        "langMap":{
+            "nolink":{
+                "desc": "Send went no input",
+                "vi_VN": "Vui lòng nhập link video Tiktok!",
+                "en_US": "Please enter the Tiktok video link!",
+                "args": {}
+            },
+            "done":{
+                "desc": "Done",
+                "vi_VN": ["Successfully!! \n Tên tài khoản Tiktok: ", " \n Username: ", " \n Số lượt xem: ", " \n Số lượt thích: ", " \n Số comments: ", " \n Số lượt chia sẻ: ", " \n Cảm ơn bạn đã sử dụng bot của tớ!"],
+                "en_US": ["Successfully!! \n Tiktok account name: ", " \n Username: ", " \n Views: ", " \n Likes: ", " \n Comments: ", " \n Shares: ", " \n Thank you for using my bot!"],
+                "args": {}
+            }
+        }
     }
 }
-var test = async function(data, api){
-    if(data.mentions == undefined) return api.sendMessage("hãy tag 1 người");
-    if(data.body == data.mentions[Object.keys(data.mentions)[0]]) return api.sendMessage("nickname trống");
-    api.changeNickname(data.body[0] == "@" ? data.body.slice(data.mentions[Object.keys(data.mentions)[0]].length - 1, data.body.length) : data.body.slice(0, data.body.indexOf(data.mentions[Object.keys(data.mentions)[0]])), data.threadID, Object.keys(data.mentions)[0])
+async function main(data, api) {
+    try {
+        const ttdl = require("tiktok-video-downloader");
+        const axios = require('axios');
+        const fs = require('fs-extra');
+        const path = require('path');
+        const link = data.args[1];
+        
+        let lang = global.lang["Download Tiktok"];
+    	let code = global.config.bot_info.lang;
+    	
+        if (!link) return api.sendMessage(lang.nolink[code], data.threadID, data.messageID);
+        const result = await ttdl.getInfo(link);
+        console.log(result);
+        var name = result.author.name;
+        var username = result.author.username;
+        var views = result.video.views;
+        var loves = result.video.loves;
+        var comments = result.video.comments;
+        var shares = result.video.shares
+        if(!result.video.url.no_wm) return api.sendMessage(lang.nolink[code], data.threadID, data.messageID);
+        const response = await axios({
+            method: 'get',
+            url: result.video.url.no_wm,
+            responseType: 'stream'
+        });
+        
+        let dir = path.join(__dirname, "temp", "cache", "tiktok", data.messageID+".mp4")
+        ensureExists(path.join(__dirname, "temp", "cache", "tiktok"))
+
+        let stream = response.data.pipe(fs.createWriteStream(dir));
+        stream.on("finish", () => {
+        	let done = lang.done[code];
+            api.sendMessage(({
+                body: done[0]+name+done[1]+username+done[2]+views+done[3]+loves+done[4]+comments+done[5]+shares+done[6],
+                attachment: fs.createReadStream(dir)
+            }), data.threadID, ()=>fs.unlinkSync(dir), data.messageID);
+        });
+    } catch (err) {
+        console.error(err);
+        api.sendMessage(err, data.threadID, data.messageID)
+    };
+};
+
+function ensureExists(path, mask) {
+    var fs = require('fs');
+  if (typeof mask != 'number') {
+    mask = 0o777;
+  }
+  try {
+    fs.mkdirSync(path, {
+      mode: mask,
+      recursive: true
+    });
+    return;
+  } catch (ex) {
+    return {
+      err: ex
+    };
+  }
 }
 
 
 module.exports = {
-    test,
+    main,
     init
 }

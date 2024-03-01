890f279cc003818c4c8b490cc67b30a7ba4b9d28
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2024-03-01 17:26:26 +0700

    idk

--- PluginStorage/DLTiktok.js
+++ PluginStorage/DLTiktok.js
@@ -40,7 +40,7 @@ function init() {
         },
         "chathook": "bruh",
         "author": "Yuuki",
-        "version": "0.1.1",
+        "version": "0.1.2",
         "nodeDepends": {
             "nayan-media-downloader": "2.0.4",
             "@tobyg74/tiktok-api-dl": "1.0.14"

3378cc6f15f702bfa1c8a55c8e18d8a9051361c7
Yuki Kiriha <87764627+reineowo@users.noreply.github.com>
2025-03-10 12:30:45 +0700

    idk

--- PluginStorage/Gemini.js
+++ PluginStorage/Gemini.js
@@ -16,7 +16,7 @@ function init() {
                     "vi_VN": "Hỏi Gemini một câu nào đó.",
                     "en_US": "Ask Gemini a question."
                 },
-                "mainFunc": "cmd1", 
+                "mainFunc": "cmd1",
                 "example": {
                     "vi_VN": "Xin chào",
                     "en_US": "Hello"
@@ -31,7 +31,7 @@ function init() {
                     "vi_VN": "Bật || Tắt tính năng rep tất cả tin nhắn!",
                     "en_US": "Turn on || Turn off the ability to reply to all messages!"
                 },
-                "mainFunc": "status", //function name of command 2
+                "mainFunc": "status", 
                 "example": {
                     "vi_VN": "Vietnamese example",
                     "en_US": "English example"
@@ -39,7 +39,6 @@ function init() {
             },
         },
         "nodeDepends": {
-            //List of required node_modules of the plugin
             "@google/generative-ai": ""
         },
         "langMap": {
@@ -68,10 +67,10 @@ function init() {
         },
         "config": {
             "APIKey": "YOUR_KEY",
-            "Intruction" : "YOUR_INSTRUCTION"
+            "instruction": "Your_instruction"
         },
         "chathook": "chathook",
-        "onload": "onload", 
+        "onload": "onload",
         "author": "Y2TB Team",
         "version": "0.0.1"
     }
@@ -89,7 +88,9 @@ const cacheDir = path.join(__dirname, "/cache/gemini");
 const apiKeyFile = path.join(cacheDir, "gemini.json");
 const statusFile = path.join(cacheDir, "status.json");
 const defaultApiKeys = [
-    "YOUR_KEY"
+    "APIKEY 1",
+    "APIKEY 2",
+    "APIKEY..."
 ];
 
 fs.mkdirSync(cacheDir, {
@@ -129,25 +130,10 @@ function getApiKey() {
 function createModel() {
     return genAI.getGenerativeModel({
         model: "gemini-2.0-flash",
-        systemInstruction: "YOUR_INSTRUCTION",
+        systemInstruction: "YOUR INSTRUCTION",
     });
 }
 
-// async function resetChatSessions() {
-// 
-//     for (let session of chatSessions.values()) {
-//         try {
-//             if (session && typeof session.close === 'function') {
-//                 await session.close();
-//             } else {
-//                 console.warn("err");
-//             }
-//         } catch (error) {
-//             console.error("err:", error);
-//         }
-//     }
-//     chatSessions.clear();
-// }
 
 function updateApiKeyUsage() {
     apiKeys.requestCounts[currentApiKeyIndex]++;
@@ -157,33 +143,28 @@ function updateApiKeyUsage() {
         apiKeys.requestCounts[currentApiKeyIndex] = 0;
         currentApiKeyIndex = (currentApiKeyIndex + 1) % apiKeys.apiKeys.length;
         console.log(`Sử dụng API Key mới: ${apiKeys.apiKeys[currentApiKeyIndex]}`);
-
-        // resetChatSessions(); // Đóng các session cũ
-
         genAI = new GoogleGenerativeAI(getApiKey());
-        model = createModel(); 
+        model = createModel();
     }
     saveApiKeys();
 }
 
 
 
-
-
 function loadStatus() {
-  try {
-    const data = fs.readFileSync(statusFile, "utf8");
-    return JSON.parse(data);
-  } catch (error) {
-    console.warn("Không tìm thấy file trạng thái, tạo file mới.");
-    fs.writeFileSync(statusFile, "{}", "utf8");
-    return {};
-  }
+    try {
+        const data = fs.readFileSync(statusFile, "utf8");
+        return JSON.parse(data);
+    } catch (error) {
+        console.warn("Không tìm thấy file trạng thái, tạo file mới.");
+        fs.writeFileSync(statusFile, "{}", "utf8");
+        return {};
+    }
 }
 
 function saveStatus() {
     fs.writeFileSync(statusFile, JSON.stringify(global.data.geminiStatus, null, 2), "utf8");
-  }
+}
 
 async function status(data, api, adv) {
     let { rlang, config, getThreadInfo } = adv;
@@ -213,7 +194,7 @@ async function status(data, api, adv) {
         saveStatus();
     } else {
         if (currentStatus) {
-            return api.sendMessage("Gemini is now on!", threadID, data.messageID); 
+            return api.sendMessage("Gemini is now on!", threadID, data.messageID);
         } else {
             global.data.geminiStatus[threadID] = true;
             if (!chatSessions.has(threadID)) {
@@ -265,25 +246,83 @@ async function cmd1(data, api, adv) {
     }
 }
 
-async function chathook(data, api, adv) {
-    if (data.type != "message") return;
 
-    let {
-        rlang,
-        config,
-        replaceMap
-    } = adv;
+function fileToGenerativePart(path, mimeType) {
+    const fs = require('fs');
+    return {
+        inlineData: {
+            data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+            mimeType,
+        },
+    };
+}
+
+async function downloadImage(url, filePath) {
+    const fs = require('fs');
+    const axios = require('axios');
+    const path = require('path');
+    const response = await axios({ url, method: 'GET', responseType: 'stream' });
+    const writer = fs.createWriteStream(filePath);
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+}
+
+
+async function chathook(data, api, adv) {
+    const axios = require('axios');
+    const fs = require('fs-extra');
+    const path = require('path');
+
+    if (data.type !== "message") return;
+
+    let { rlang, config, replaceMap } = adv;
 
     !global.data.geminiStatus ? global.data.geminiStatus = {} : '';
-    global.data.geminiStatus[data.threadID] == undefined ? global.data.geminiStatus[data.threadID] = config.geminiStatus : '';
+    global.data.geminiStatus[data.threadID] === undefined ? global.data.geminiStatus[data.threadID] = config.geminiStatus : '';
 
     if (!global.data.geminiStatus[data.threadID]) return;
-    if (data.body.indexOf(global.config.facebook.prefix) == 0) return;
+    if (data.body.indexOf(global.config.facebook.prefix) === 0) return;
 
     const threadID = data.threadID;
     const chatSession = chatSessions.get(threadID);
 
     if (!chatSession) {
+        return;
+    }
+    if (data.attachments && data.attachments.length > 0) {
+        const attachment = data.attachments[0];
+        const link = attachment.url;
+        let dir;
+        let mimeType;
+
+        if (attachment.type === "photo") {
+            dir = path.join(__dirname, "cache", "gemini", "gemini.jpg");
+            mimeType = "image/jpg";
+        } else if (attachment.type === "video") {
+            dir = path.join(__dirname, "cache", "gemini", "gemini.mp4");
+            mimeType = "video/mp4";
+        } else if (attachment.type === "audio") {
+             dir = path.join(__dirname, "cache", "gemini", "gemini.mp3");
+             mimeType = "audio/mp3";
+        } else {
+            return; 
+        }
+        ensureExists(path.join(__dirname, "cache", "gemini"));
+        await downloadImage(link, dir);
+        const prompt = data.body;
+        const attachmentPart = fileToGenerativePart(dir, mimeType);
+        try {
+            const result = await chatSession.sendMessage([prompt, attachmentPart]);
+            api.sendMessage(result.response.text(), data.threadID, data.messageID);
+        } catch (error) {
+            console.error("Lỗi khi gọi Gemini API:", error);
+            api.sendMessage("Có lỗi xảy ra khi xử lý yêu cầu của bạn. Vui lòng thử lại sau.", data.threadID, data.messageID);
+        } finally {
+            updateApiKeyUsage();
+        }
         return;
     }
 
@@ -298,27 +337,46 @@ async function chathook(data, api, adv) {
         updateApiKeyUsage();
     }
 }
+
 function onload(info) {
-    global.data.geminiStatus = loadStatus(); 
+    global.data.geminiStatus = loadStatus();
 
 
-  for (const threadID in global.data.geminiStatus) {
-    if (global.data.geminiStatus[threadID]) {
-      chatSessions.set(threadID, model.startChat({
-        generationConfig: {
-          temperature: 1,
-          topP: 0.95,
-          topK: 40,
-          maxOutputTokens: 8192,
-          responseMimeType: "text/plain",
-        },
-        history: [],
-      }));
+    for (const threadID in global.data.geminiStatus) {
+        if (global.data.geminiStatus[threadID]) {
+            chatSessions.set(threadID, model.startChat({
+                generationConfig: {
+                    temperature: 1,
+                    topP: 0.95,
+                    topK: 40,
+                    maxOutputTokens: 8192,
+                    responseMimeType: "text/plain",
+                },
+                history: [],
+            }));
+        }
     }
-  }
 
 }
 
+
+function ensureExists(path, mask) {
+    var fs = require('fs');
+    if (typeof mask != 'number') {
+        mask = 0o777;
+    }
+    try {
+        fs.mkdirSync(path, {
+            mode: mask,
+            recursive: true
+        });
+        return;
+    } catch (ex) {
+        return {
+            err: ex
+        };
+    }
+}
 
 module.exports = {
     cmd1,

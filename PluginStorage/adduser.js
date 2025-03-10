4189bb2b82b405a55b236b0785a8f2be31c02476
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2025-03-10 20:58:38 +0700

    idk

--- PluginStorage/adduser.js
+++ PluginStorage/adduser.js
@@ -12,7 +12,7 @@ function init() {
             "adduser": {
                 "help": {
                     "vi_VN": "<url hoặc uid facebook>",
-                    "en_US": "url or uid facebook"
+                    "en_US": "<url or uid facebook>"
                 },
                 "tag": {
                     "vi_VN": "Thêm thành viên bằng url hoặc uid facebook",
@@ -20,41 +20,49 @@ function init() {
                 },
                 "mainFunc": "cmd1", 
                 "example": {
-                    "vi_VN": "https://www.facebook.com/me?",
-                    "en_US": "https://www.facebook.com/me?"
+                    "vi_VN": "adduser https://www.facebook.com/me?",
+                    "en_US": "adduser https://www.facebook.com/me?"
                 }
             },
           
         },
-        "nodeDepends": {
-        },
         "langMap": {
-            "translation code 1": {
-                "desc": "Describe the function of translation",
-                "vi_VN": "Translation in Vietnamese {0}",
-                "en_US": "Translation in English {0}",
-                "args": {
-                    "{0}": {
-                        "vi_VN": "Describe the function of the variable in Vietnamese",
-                        "en_US": "Describe the function of the variable in English"
-                    }
-                }
+            "noLink": {
+                "desc": "Error message when user does not enter valid user link",
+                "vi_VN": "Vui lòng nhập url Facebook!",
+                "en_US": "Please enter facebook url!",
+                "args": {}
             },
-            "translation code 2": {
-                "desc": "Describe the function of translation",
-                "vi_VN": "Translation in Vietnamese {1}",
-                "en_US": "Translation in English {1}",
-                "args": {
-                    "{1}": {
-                        "vi_VN": "Describe the function of the variable in Vietnamese",
-                        "en_US": "Describe the function of the variable in English"
-                    }
-                }
+            "invalidUser": {
+                "desc": "Error message when invalid user link",
+                "vi_VN": "Link người dùng hoặc UID không hợp lệ!",
+                "en_US": "Invalid user link or UID!",
+                "args": {}
+            },
+            "present": {
+                "desc": "Notify when there are members in the group",
+                "vi_VN": "Thành viên này đã có trong nhóm!",
+                "en_US": "This member is already in the group!",
+                "args": {}
+            },
+            "addPending": {
+                "desc": "Notify when there are members in the group",
+                "vi_VN": "Đã thêm vào danh sách duyệt thành viên!",
+                "en_US": "Added to member watchlist!",
+                "args": {}
+            },
+            "cantAdd": {
+                "desc": "Notify when cannot add user to this group",
+                "vi_VN": "Không thể thêm thành viên vào nhóm!",
+                "en_US": "Cannot add user to this group!",
+                "args": {}
+            },
+            "done": {
+                "desc": "Notification when successfully added to group",
+                "vi_VN": "Đã thêm vào nhóm thành công!",
+                "en_US": "Added to group successfully!",
+                "args": {}
             }
-        },
-        "config": {
-            "config 1": "value 1",
-            "config 2": "value 2"
         },
         "author": "ReineOwO",
         "version": "0.0.1"
@@ -62,11 +70,12 @@ function init() {
 }
 
 async function cmd1(data, api, adv) {
-    const axios = require("axios")
-    const fetch = require("node-fetch");
+    const {rlang, replaceMap} = adv;
+    const axios = require("axios");
+
     async function getFacebookUID(link, api, data) {
         if (!link) {
-            return api.sendMessage("Please enter facebook url!", data.threadID, data.messageID);
+            return api.sendMessage(rlang("noLink"), data.threadID, data.messageID);
         }
         const apiUrl = "https://fbuid.mktsoftware.net/api/v1/fbprofile?url=" + link;
         try {
@@ -77,7 +86,7 @@ async function cmd1(data, api, adv) {
                 for (let i in res.participantIDs) {
                     let x = res.participantIDs[i];
                     if (x == uid) {
-                        return api.sendMessage("Thành viên này đã có trong nhóm!", data.threadID, data.messageID)
+                        return api.sendMessage(rlang("present"), data.threadID, data.messageID)
                     }
                 }
                 var adminis = false;
@@ -98,12 +107,12 @@ async function cmd1(data, api, adv) {
                                 }
                             });
                         });
-                        api.sendMessage("Đã thêm vào danh sách duyệt thành viên!", data.threadID, data.messageID);
+                        api.sendMessage(rlang("addPending"), data.threadID, data.messageID);
 
                     } catch (error) {
-                        console.error("Lỗi thêm vào nhóm:", error);
-                        const errorMessage = error.message ? error.message : "Lỗi không xác định khi thêm vào nhóm!";
-                        api.sendMessage("Lỗi khi thêm vào nhóm: " + errorMessage, data.threadID, data.messageID);
+                        console.error("adduser", error);
+                        const errorMessage = error.message ? error.message : rlang("cantAdd");
+                        api.sendMessage(errorMessage, data.threadID, data.messageID);
                     }
                     return;
                 }
@@ -124,13 +133,13 @@ async function cmd1(data, api, adv) {
 
             } catch (error) {
                 console.error("Lỗi thêm vào nhóm:", error);
-                const errorMessage = error.message ? error.message : "Lỗi không xác định khi thêm vào nhóm!";
-                api.sendMessage("Lỗi khi thêm vào nhóm: " + errorMessage, data.threadID, data.messageID);
+                const errorMessage = error.message ? error.message : rlang("cantAdd");
+                api.sendMessage(errorMessage, data.threadID, data.messageID);
             }
 
         } catch (error) {
-            console.error("Lỗi lấy dữ liệu người dùng:", error);
-            const errorMessage = error.message ? error.message : "Không thể lấy dữ liệu người dùng. Vui lòng kiểm tra lại URL Facebook!";
+            console.error("adduser", error);
+            const errorMessage = error.message ? error.message : rlang("invalidUser");
             api.sendMessage(errorMessage, data.threadID, data.messageID);
         }
     }
@@ -145,7 +154,7 @@ async function cmd1(data, api, adv) {
     } else if (/^\d+$/.test(link)) {
         return getFacebookUID("https://www.facebook.com/" + link, api, data);
     } else {
-        return api.sendMessage("Vui lòng nhập uid hoặc url facebook!", data.threadID, data.messageID)
+        return api.sendMessage(rlang("noLink"), data.threadID, data.messageID);
     }
 
 

1fc1714a82fbbc492d64e2bd7e7bf0778b0c8db5
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2025-04-06 13:17:02 +0700

    idk

--- PluginStorage/Youtube.js
+++ PluginStorage/Youtube.js
@@ -463,6 +463,7 @@ async function downmp3(data, api, { rlang, replaceMap, config }, link) {
         if (fs.statSync(dirr).size > 26214400) {
             api.sendMessage(rlang("more25mb"), data.threadID, () => fs.unlinkSync(dirr), data.messageID);
         } else {
+            console.log(fs.createReadStream(dirr))
             api.sendMessage({
                 attachment: fs.createReadStream(dirr)
             }, data.threadID, () => fs.unlinkSync(dirr), data.messageID);
